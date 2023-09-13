5d56e70392a96a46a504f2fc1dcd21af5a007cd9
莫压我腿毛 <2743005451@qq.com>
2023-09-13 19:45:01 +0800

    add do

---
+++ Skill/Skill.cs
@@ -0,0 +1,23 @@
+void Create()
+{
+    var entity=MongoHeper.Instance.Clone(this)
+    entity.Start(sence,true)
+
+    if()
+    {
+        aaa;
+    }
+    else if()
+    {
+        bbb;
+    }
+    else
+    {
+        ccc;
+    }
+}
+
+public void Do()
+{
+ 
+}
