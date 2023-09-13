05306193c4749f577362531f7a772ee1be3f076b
莫压我腿毛 <2743005451@qq.com>
2023-09-13 19:58:57 +0800

    item do

---
+++ Itme/Itme.cs
@@ -0,0 +1,24 @@
+public entity Create()
+{
+    var entity=MongoHeper.Instance.Clone(this)
+    entity.Start(sence,true)
+
+    if()
+    {
+        aaa;
+    }
+    else if()
+    {
+        bbb;
+    }
+    else
+    {
+        ccc;
+    }
+    return entity;
+}
+
+public void Do()
+{
+ 
+}
